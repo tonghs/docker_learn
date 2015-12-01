7ed0286537790bc670710b24060050e334557d02
tonghs <tonghuashuai@gmail.com>
2015-12-01 19:31:12 +0800

    fix

--- tonghs-dev-ubuntu/run.sh
+++ tonghs-dev-ubuntu/run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# service redis-server start
-# nginx
-
 /usr/sbin/sshd -D
--- tonghs-dev/run.sh
+++ tonghs-dev/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-service redis-server start
-nginx
+#service redis-server start
+#nginx
 
 /usr/sbin/sshd -D
